c001e0287b6b336c892b5641025c5ea398ebc2f2
AMO15310 <wachiraamos402@gmail.com>
2023-05-12 19:33:40 +0300

    The state class

---
+++ models/state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Describes the state class
+"""
+
+from models.base_model import BaseModel
+
+class State(BaseModel):
+    """The state class
+    Attributes:
+        name (str): name of the state
+    """
+    name = ""
